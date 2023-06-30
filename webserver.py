2cf99b35f8d19f2f98e85e66f06fc80a862a75ea
Jimmie Tyrrell <jimmie@subpixel.net>
2023-06-30 11:51:32 -0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -2,19 +2,23 @@
 # Author: Jimmie Tyrrell
 # License: Unlicense
 #
-# Drop this in /usr/local/bin, and chmod o+x
+# Drop this somewhere like /usr/local/bin/webserver, then chmod o+x
 #
 # Usage:
 #   webserver # Starts a webserver in the current directory on port 8000
 #   webserver --port 9090 --path dist # Webserver on port 9090 serving content out of ./dist
-
+#   webserver --host 0.0.0.0
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+from functools import partial
 import sys
 import argparse
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
+class Handler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def end_headers(self):
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
@@ -25,13 +29,15 @@ def main():
     parser=argparse.ArgumentParser()
 
     parser.add_argument("--port", help="Port to run the webserver on (default 8000)", default=8000)
+    parser.add_argument("--host", help="Host to run the webserver on (default empty)", default=None)
     parser.add_argument("--path", help="Path to serve documents from (default .)", default=".")
 
     args=parser.parse_args()
 
-    print(f"Args: {vars(args)}")
+    print(f"WebServer starting. Args: {vars(args)}")
 
-    test(CORSRequestHandler, HTTPServer, port=int(args.port))
+    handler = partial(Handler, directory=args.path)
+    test(handler, HTTPServer, bind=args.host, port=int(args.port))
 
 if __name__ == '__main__':
     main()
